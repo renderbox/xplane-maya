660105621aa84a0b371e1c32775caa3f049861fc
Grant Viklund <gviklund@backcode.com>
2013-09-25 15:24:05 -0700

    Added new Files

---
+++ component.py
@@ -0,0 +1,46 @@
+# Basic Components for Polygons
+
+class Vertex(object):
+    ''' XPlane 10 Vertex '''
+    def __init__(self, x, y, z, nx, ny, nz, u, v, w=None):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.nx = nx
+        self.ny = ny
+        self.nz = nz
+        self.u = u
+        self.v = v
+        self.w = w
+
+    def wobj_vertex(self):
+        return "v %f %f %f" % (self.x, self.y, self.z)
+
+    def wobj_texture(self):
+        return "vt %f %f" % (self.u, self.v)
+
+    def wobj_normals(self):
+        return "vn %f %f %f" % (self.nx, self.ny, self.nz)
+
+
+class Polygon(object):
+    '''Polygon object for Managing poly surfaces'''
+
+    def __init__(self, points):
+        self.points = points
+
+    def wobj_face(self):
+        return "f %s" % ' '.join(self.points)
+
+    def wobj_face_full(self):
+        return "f %s" % ' '.join( ["%d/%d/%d" % (x, x, x) for x in self.points] )
+
+
+class Geometry(object):
+    '''Geometry Object for managing Collections of Polys'''
+    
+    def __init__(self):
+        self.rawdata = []
+        self.vertexList = []
+        self.pointList = []
+        self.polyList = []
---
+++ io.py
@@ -0,0 +1,70 @@
+
+
+# File IO
+
+from component import Geometry
+
+class WOBJWriter(class):
+    '''Take a Geormetry Object and writes it out in Wavefront OBJ Format'''
+
+    def __init__(self, geo):
+        self.geo = geo
+
+    def write(self, dest_file, otype="wobj"):
+        '''Writes out a Wavefront OBJ by default'''
+        if otype == "wobj":
+            self.write_wobj(dest_file)
+
+    def write_wobj(self, dest_file):
+        '''Write out the object as a Wavefront OBJ file'''
+        # SETUP THE DATA
+        output_lines.extend([ v.wobj_vertex() for v in self.geo.vertexList ])
+        output_lines.extend([ v.wobj_texture() for v in self.geo.vertexList ])
+        output_lines.extend([ v.wobj_normals() for v in self.geo.vertexList ])
+        output_lines.extend([ p.wobj_face_full() for p in self.geo.polyList ])
+
+        # WRITE
+        outfile = file(dest_file,'w')
+        outfile.write('\n'.join([line for line in output_lines if line]))
+        outfile.close()
+
+
+class XPlaneOBJReader(class):
+    '''Creates a Geormetry Object from an XPlane OBJ file'''
+
+    def __init__(self):
+        self.geo = None
+
+    def read(self, src_file):
+        '''Will read in the file from the given model path'''
+        self.geo = Geometry     # RESET IT TO NEW EVERYTIME IT'S READ TO MAKE SURE IT IS A CLEAN READ
+
+        f = open(src_file)
+        self.geo.rawdata = [x.strip() for x in f.readlines()]
+        f.close()
+        self.geo.vertexList = []
+        self.geo.pointList = []
+        self.geo.polyList = []
+        self.geo.output_lines = ["g default"]
+
+        # LOAD THE VERTECIES, UVs AND INDEXES
+        for line in self.geo.rawdata:
+            parts = line.split()
+
+            if not parts:
+                continue
+
+            if parts[0] == 'VT':
+                args = [float(x) for x in parts[1:]]
+                self.geo.vertexList.append(Vertex(*args))
+
+            if parts[0][:3] == 'IDX':
+                self.geo.pointList.extend([int(x) for x in parts[1:] ])
+
+        # LOAD THE POLYGONS
+        i = 0
+        while i<len(pointList):
+            self.geo.polyList.append( Polygon( [ x+1 for x in pointList[i:i+3]] ) )
+            i += 3  # ASSUMES 3-POINT POLYS
+
+        return self.geo
