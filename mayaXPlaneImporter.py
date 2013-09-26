72014d54e2e6e702e62ea686aad187de5ebe7174
Grant Viklund <gviklund@backcode.com>
2013-09-25 17:32:25 -0700

    Added sample code for the importer command

--- mayaXPlaneImporter.py
+++ mayaXPlaneImporter.py
@@ -1,6 +1,7 @@
 import maya.OpenMaya as OpenMaya
 import maya.OpenMayaMPx as OpenMayaMPx
 import sys
+from io import XPlaneOBJReader
 
 # Initialize the script plug-in
 def initializePlugin(mobject):
@@ -26,68 +27,56 @@ class scriptedCommand(OpenMayaMPx.MPxCommand):
         OpenMayaMPx.MPxCommand.__init__(self)
 
     def doIt(self, argList):
-        print "Hello World!"
         print argList
         print "ARG LIST ABOVE"
 
+        xpreader = XPlaneOBJReader()
+        geoObj = xpreader.read("")      #PATH TO FILE FROM ARGS
 
-# import maya.OpenMaya
+        outputMesh = maya.OpenMaya.MObject()
+        numFaces = len( geoObj.polyList )
+        numVertices = len( geoObj.vertexList )
 
-# def pk_makePolyPlane(width=10.0,length=10.0,subX=15,subZ=15):
-#     '''pk_makePolyPlane(width=10.0,length=10.0,subX=15,subZ=15)
-#     is a python script in the file
-#     C:\Documents and Settings\Paul\My Documents\maya\2008\python\pythonlearning.py
-#     Creates a poly plane centered at the origin of the world space. '''
+        print "VERT COUNT: %d" % numVertices
+        print "FACE COUNT: %d" % numFaces
 
-#     outputMesh = maya.OpenMaya.MObject()
+        UVSetNames = ['map1', 'other']
 
-#     nRows = subX + 1
-#     nCols = subZ + 1
+        points = maya.OpenMaya.MFloatPointArray()
+        uArray = maya.OpenMaya.MFloatArray()
+        vArray = maya.OpenMaya.MFloatArray()
 
-#     numFaces = subX * subZ
-#     numVertices = nRows * nCols
-#     # point array of plane vertex local positions
-#     points = maya.OpenMaya.MFloatPointArray()
-#     for x in range(0,nRows,1):
-#         for z in range(0,nCols,1):
-#             px = width*(x/float(subX)) - width/2.0
-#             py = 0.0
-#             pz = length*(z/float(subZ)) - length/2.0
-#             p = maya.OpenMaya.MFloatPoint( px, py, pz )
-#             #print 'point:: %f, %f, %f' % (p.x, p.y, p.z)
-#             points.append(p)
+        # CONVERT THE POINTS TO MAYA FORMAT
+        print "CREATING VERTICIES"
+        for vertex in geoObj.vertexList:
+            p = maya.OpenMaya.MFloatPoint( vertex.x, vertex.y, vertex.z )
+            points.append(p)
+            uArray.append(vertex.u)
+            vArray.append(vertex.v)
 
-#     # vertex connections per poly face in one array of indexs into point array given above
-#     faceConnects = maya.OpenMaya.MIntArray()
-#     for row in range(0,subX,1):
-#         for col in range(0,subZ,1):
-#             #fID = (row * subX) + (col % subZ)
-#             #print 'row:%i, col:%i, polyFaceID:%i' % (row,col,fID) 
-#             index0 = ((row % nRows) * nCols) + (col % nCols)
-#             faceConnects.append(index0)
-#             index1 = ((row % nRows) * nCols) + ((col+1) % nCols)
-#             faceConnects.append(index1)
-#             index2 = (((row+1) % nRows) * nCols) + ((col+1) % nCols)
-#             faceConnects.append(index2)
-#             index3 = (((row+1) % nRows) * nCols) + (col % nCols)
-#             faceConnects.append(index3)
-#             #print 'face vertex ID: %i, %i, %i, %i' % (index0,index1,index2,index3)
+        # vertex connections per poly face in one array of indexs into point array given above
+        faceConnects = maya.OpenMaya.MIntArray()
+        for point in geoObj.pointList:
+            faceConnects.append(point)
 
-#     # an array to hold the total number of vertices that each face has
-#     faceCounts = maya.OpenMaya.MIntArray()
-#     for c in range(0,numFaces,1):
-#         faceCounts.append(4)
+        # an array to hold the total number of vertices that each face has
+        faceCounts = maya.OpenMaya.MIntArray()
+        for poly in geoObj.polyList:
+            faceCounts.append( len( poly.points ) )
 
-#     #create mesh object using arrays above and get name of new mesh
-#     meshFS = maya.OpenMaya.MFnMesh()
-#     newMesh = meshFS.create(numVertices, numFaces, points, faceCounts, faceConnects, outputMesh)
-#     meshFS.updateSurface()
-#     nodeName = meshFS.name()
-#     print 'Mesh node name is: %s' % nodeName
+        meshFS = maya.OpenMaya.MFnMesh()
+        #newMesh = meshFS.create(numVertices, numFaces, points, faceCounts, faceConnects, outputMesh)
+        newMesh = meshFS.create(numVertices, numFaces, points, faceCounts, faceConnects, uArray, vArray, outputMesh)    # ADDING THE UVs TO THE MESH AT THE SAME TIME
+        meshFS.updateSurface()
+        nodeName = meshFS.name()
+        print 'Mesh node name is: %s' % nodeName
 
-#     #assign new mesh to default shading group
-#     maya.cmds.sets (nodeName, e=True, fe='initialShadingGroup')
-#     return nodeName
+        # CREATE THE UV SET                             # TRYING TO REPLACE THIS WITH THE create METHOD ABOVE
+        #meshFS.createUVSetWithName( UVSetNames[1] )
+        #meshFS.setUVs(uArray, vArray, UVSetNames[1] )
+
+        #assign new mesh to default shading group
+        maya.cmds.sets(nodeName, e=True, fe='initialShadingGroup')
 
 
-# pk_makePolyPlane(width=10.0,length=10.0,subX=15,subZ=15)
+
